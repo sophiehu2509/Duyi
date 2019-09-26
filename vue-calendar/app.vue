52c66f31c9fd0ccbd85d9892ecb63acfc7f2312f
Ye Hu <sophiehu2509@users.noreply.github.com>
2019-09-26 12:58:33 -0400

    vue-calendar

---
+++ vue-calendar/app.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <calendar />
+    </div>
+</template>
+
+<script>
+import Calendar from './calendar';
+
+export default {
+    data(){
+        return {
+            msg:'hello world'
+        }
+    },
+    components:{
+        Calendar
+    }
+}
+</script>
+
+<style>
+div{
+    /* background-color:red; */
+    color:#000;
+}
+</style>
